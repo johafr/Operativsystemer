e829330453ed88414fa47364af5e4e6a28c1a4bf
Johannes Finne Reinseth <johafr@stud.ntnu.no>
2022-02-09 15:55:41 +0100

    attempt time_t

--- OSoving1.c
+++ OSoving1.c
@@ -9,26 +9,46 @@
 //structs
 struct alarm {
   int alarmId;
-  int PID; //Stod at vi kunne trenge det i oppgaven
+  int PID; 
+  time_t ringTime;
 };
 
 struct alarm alarms[10];
 
 //methods
-void actionL() {
-  
+void actionS() {
+  struct tm *alarmTime;
+  printf("When should the alarm go off? (format: yyyy-mm-dd hh:mm:ss): ");
+  scanf("%s", alarmTime);
+  time_t rawTime = mktime(&alarmTime);
+  for (int i = 0; i < 10; i++) {
+    if (alarms[i].alarmId == NULL) {
+      alarms[i].alarmId = i + 1;
+      alarms[i].ringTime = rawTime;
+
+      break;
+    }
+  }
+  printf("alarm goes off %s: ", rawTime);
 }
 
+
+//main
 int main(){
+  time_t rawTime;
+  struct tm *timeinfo;
+  time (&rawTime);
+  timeinfo = localtime (&rawTime);
+
   char action; 
-  char* menu =  "Welcome to the alarm clock! It is currently 'insert time'. Please enter 's' (schedule), 'l' (list), 'c' (cancel), 'x' (exit) \n";
-  printf(" %s \n", menu);
+  printf("Welcome to the alarm clock! It is currently %s . Please enter 's' (schedule), 'l' (list), 'c' (cancel), 'x' (exit) \n", asctime (timeinfo));
  
   while (action != 'x') {
     scanf(" %c", &action); 
     
     if (action == 's') {
       printf("your input = %c \nyou can schedule an alarm \n", action);
+      actionS();
     } else if (action == 'l') {
       printf("your input = %c \nyou can display alarms \n" , action);
     } else if (action == 'c') {
