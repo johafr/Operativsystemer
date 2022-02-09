64028c42017cb8e1a917028115a8fc91fc5f11fa
Johannes Finne Reinseth <johafr@stud.ntnu.no>
2022-02-09 09:18:47 +0100

    alarm struct

--- OSoving1.c
+++ OSoving1.c
@@ -1,2 +1,24 @@
 //Unix based alarm clock
+#include<stdio.h>
+#include<time.h>
 
+typedef struct alarm {
+  int alarmId;
+  int PID; //Stod at vi kunne trenge det i oppgaven
+  struct alarm *next;
+}alarm;
+
+alarm *createAlarm(int alarmId) {
+  alarm *newAlarm = malloc(sizeof(alarm));
+  newAlarm -> alarmId = alarmId;
+  newAlarm -> next = NULL;
+  return newAlarm;
+}
+
+
+
+int main() {
+
+  printf("Compiled");
+  return 0;
+}
