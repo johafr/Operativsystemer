276aeafc641fb84b5dabb6d8ada875113a4066e8
Johannes Finne Reinseth <johafr@stud.ntnu.no>
2022-02-23 16:05:36 +0100

    cancel action

--- OSoving1.c
+++ OSoving1.c
@@ -23,35 +23,54 @@ void actionS() {
   time_t currentTime;
   time(&currentTime);
   
-  struct tm *alarm = localtime(&currentTime);
+  struct tm *alarm;
   
-  char input[250];
+  char date[250];
+  int year;
+  int month;
+  int day;
   printf("enter scheduled date (YYYY-MM-DD): ");
-  scanf("%s", &input);
-  sscanf(input, "%4d-%2d-%2d", &alarm->tm_year - 1900, &alarm->tm_mon - 1, &alarm->tm_mday);
+  scanf("%s", &date);
+  sscanf(date, "%4d-%2d-%2d", &year, &month, &day);
+
+  alarm->tm_year = year - 1900;
+  alarm->tm_mon = month - 1;
+  alarm->tm_mday = day;
+
+  char time[250];
+  int hour;
+  int minute;
+  int second;
   
   printf("enter scheduled time (hh:mm:ss): ");
-  scanf("%s", &input);
-  sscanf(input, "%2d:%2d:%2d", &alarm->tm_hour, &alarm->tm_min, &alarm->tm_sec);
+  scanf("%s", &time);
+  sscanf(time, "%2d:%2d:%2d", &hour, &minute, &second);
 
-  printf("the year is: %4d\n", alarm->tm_year);
-  printf("the month is: %2d\n", alarm->tm_mon);
+  alarm->tm_hour = hour;
+  alarm->tm_min = minute;
+  alarm->tm_sec = second;
+
+  printf("the year is: %4d\n", alarm->tm_year + 1900);
+  printf("the month is: %2d\n", alarm->tm_mon + 1);
   printf("the day is: %2d\n", alarm->tm_mday);
   printf("the hour is: %2d\n", alarm->tm_hour);
   printf("the second is: %2d\n", alarm->tm_min);
   printf("the second is: %2d\n", alarm->tm_sec);
 
   time_t alarmTime = mktime(alarm);
-  printf("Scheduling alarm for: %s\n", ctime(&alarmTime));
-  for (int i = 0; i < 10; i++) {
-    if (alarms[i].PID == 0) {
-      alarms[i].ringTime = alarmTime;
-      alarms[i].PID = i;
-      break;
-    }
+  if (alarmTime - currentTime > 0) {
+    printf("Scheduling alarm for: %s\n", ctime(&alarmTime));
+        for (int i = 0; i < 10; i++) {
+          if (alarms[i].PID == 0) {
+            alarms[i].ringTime = alarmTime;
+            alarms[i].PID = i + 1;
+            break;
+          }
+        }
+        printf("Scheduling alarm in %ld seconds\n", alarmTime - currentTime);
+  } else {
+    printf("Invalid input, try again. \n");
   }
-
-  printf("%ld", alarmTime - currentTime);
 }
 
 void actionL() {                                              // Skal komme med en liste over alle alarmer 
@@ -59,13 +78,30 @@ void actionL() {                                              // Skal komme med 
   time(&currentTime);                                         // Setter tiden akkurat nå
   printf("Your alarms are set at: \n");
   for (int i = 0; i<10; i++){
-    time_t test2 = alarms[i].ringTime;
-    printf("%s \n", ctime(&test2));
+    time_t alarmTime = alarms[i].ringTime;
+    if (alarms[i].PID != 0) {
+      printf("%i %s \n",alarms[i].PID ,ctime(&alarmTime));
+    } 
   }
 }
 
 void actionC() {
-  
+  int index;
+  printf("enter the number of the alarm you wish to cancel: \n");
+  scanf("%i", &index);
+  int deleted = 0;
+  for (int i = 0; i < 10; i++) {
+    if (alarms[i].PID == index) {
+      alarms[i].PID = 0;
+      alarms[i].ringTime = time(NULL);
+      deleted = 1;
+    }
+  }
+  if (deleted == 0) {
+    printf("Alarm does not exist. \n");
+  } else {
+    printf("Alarm %i is deleted. ", index);
+  }
 }
 
 void actionX() {
